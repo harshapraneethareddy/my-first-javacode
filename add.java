2b7a9f6501eb8262e5b6281ec4cbc40079075244
04sanjayKR <43778570+04sanjayKR@users.noreply.github.com>
2021-03-25 11:56:59 +0530

    Update add.java

--- add.java
+++ add.java
@@ -2,8 +2,10 @@ public class add
 {
 public static void main(String[] args)
 {
-int num1 = 5, num2 = 9, sum;
+int num1 = 5, num2 = 9, sum,sub;
 sum = num1 + num2;
+ sub=num2-num1;
+ System.out.println("sum of these two numbers: "+sub); 
 System.out.println("sum of these two numbers: "+sum);
 }
 }
